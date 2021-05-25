71dded06ab5cf7f1e29b5208ecaba16b072e99f8
WhitesLove <kubai.rita2@yandex.ru>
2021-05-25 23:29:43 +0300

    Sync

--- src/main/java/com/example/demo/controller/LoadController.java
+++ src/main/java/com/example/demo/controller/LoadController.java
@@ -1,19 +1,19 @@
-package com.example.demo.controller;
-
-import com.example.demo.component.DataLoader;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/load")
-public class LoadController {
-    private final DataLoader dataLoader;
-
-    @GetMapping
-    public void load() {
-        dataLoader.load();
-    }
-}
+//package com.example.demo.controller;
+//
+//import com.example.demo.component.DataLoader;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/load")
+//public class LoadController {
+//    private final DataLoader dataLoader;
+//
+//    @GetMapping
+//    public void load() {
+//        dataLoader.load();
+//    }
+//}
