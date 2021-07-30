f89d8f3a2037ab658c0e0b0ef0b026a2e7023cba
WhitesLove <kubai.rita2@yandex.ru>
2021-07-30 21:28:35 +0300

    v. 2.4.1 - update backend.
    Update security.

--- src/main/java/com/example/demo/controller/LoadController.java
+++ src/main/java/com/example/demo/controller/LoadController.java
@@ -1,19 +1,29 @@
-//package com.example.demo.controller;
-//
-//import com.example.demo.component.DataLoader;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequiredArgsConstructor
-//@RestController
-//@RequestMapping("/load")
-//public class LoadController {
-//    private final DataLoader dataLoader;
-//
-//    @GetMapping
-//    public void load() {
-//        dataLoader.load();
-//    }
-//}
+package com.example.demo.controller;
+
+import com.example.demo.component.DataLoader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/load")
+public class LoadController {
+    @Value( "${sw-db.load-password}" )
+    private final String loadPassword;
+    private final DataLoader dataLoader;
+
+    @GetMapping
+    public void load(@RequestParam String password) {
+        if (!password.equals(loadPassword)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        dataLoader.load();
+    }
+}
